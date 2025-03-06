954334283cf63234fd9f768ef265ee2eed0ed04e
joneshshrestha <contact@joneshshrestha.com>
2025-03-06 14:20:38 -0600

    new project COR and Decorator design pattern

---
+++ CoR and Decorator Pattern/src/main/java/com/joneshshrestha/designpattern/App.java
@@ -0,0 +1,10 @@
+package com.joneshshrestha.designpattern;
+
+public class App {
+
+	public static void main(String[] args) {
+		ConverterGUI ui = new ConverterGUI();
+
+	}
+
+}
---
+++ CoR and Decorator Pattern/src/main/java/com/joneshshrestha/designpattern/Client.java
@@ -0,0 +1,11 @@
+package com.joneshshrestha.designpattern;
+
+public class Client {
+
+	//TODO Here is where you implement the converter logic using the requested "patterns" (Chain of Responsibility, and Decorator)
+	//TODO You can have a static method Convert(..) here
+	//TODO static Convert(...) method?
+	//TODO In the method body you must setup your patterns like Instantiating Handlers and setSuccessors for the Chain of Responsibility
+	//TODO and Decorators for the Decorator pattern
+
+}
